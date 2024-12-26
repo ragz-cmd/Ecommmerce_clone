61ea211e1dc1836f577c9c26c03077a5f25ecae4
ragz-cmd <anuragmokashi01@gmail.com>
2024-12-26 22:02:26 +0530

    login and signup page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const isAdmin = false;
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-slate-200 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-stone-400">
+    <header className="fixed top-0 left-0 w-full bg-slate-300 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-stone-400">
       <div className="container mx-auto px-4 py-3">
         <div className="flex flex-wrap justify-between items-center">
           <Link
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,151 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import {
+  LogIn,
+  Loader,
+  Lock,
+  ArrowRight,
+  Mail,
+  Eye,
+  EyeOff,
+} from "lucide-react";
 const Login = () => {
-  return <div>Login</div>;
+  const loading = false;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <motion.div
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2 className="mt-6  font-montserrat text-center text-3xl font-extrabold text-slate-700">
+          Create your account
+        </h2>
+      </motion.div>
+
+      <motion.div
+        className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <div className="bg-gray-400 py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email address
+              </label>
+              <div className="mt-1 relative rounded-md shadow-sm">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="h-5 w-5 text-black" aria-hidden="true" />
+                </div>
+                <input
+                  id="email"
+                  type="email"
+                  required
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                  className=" text-black font-medium block w-full px-3 py-2 pl-10 bg-slate-100 border border-slate-200 rounded-md shadow-sm
+               placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                  placeholder="you@example.com"
+                />
+              </div>
+            </div>
+
+            <div className="mt-1 relative rounded-md shadow-sm">
+              {/* Lock Icon on the Left */}
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <Lock className="h-5 w-5 text-black" aria-hidden="true" />
+              </div>
+
+              {/* Password Input */}
+              <input
+                id="password"
+                type={showPassword ? "text" : "password"}
+                required
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+                className="text-black font-medium block w-full px-3 py-2 pl-10 pr-10 bg-slate-100 border border-slate-200 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-black focus:border-black sm:text-sm"
+                placeholder="******"
+              />
+
+              {/* Eye Icon on the Right */}
+              <div
+                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                onClick={() => setShowPassword(!showPassword)}
+                // Ensure the cursor remains consistent
+              >
+                {showPassword ? (
+                  <Eye
+                    className="h-5 w-5  text-gray-600 hover:text-gray-400"
+                    aria-hidden="true"
+                  />
+                ) : (
+                  <EyeOff
+                    className="h-5 w-5  text-gray-600 hover:text-gray-400"
+                    aria-hidden="true"
+                  />
+                )}
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent 
+          rounded-md shadow-sm text-sm font-medium text-white bg-slate-600
+           hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2
+            focus:ring-slate-500 transition duration-150 ease-in-out disabled:opacity-50"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Loader
+                    className="mr-2 h-5 w-5 animate-spin"
+                    aria-hidden="true"
+                  />
+                  Loading...
+                </>
+              ) : (
+                <>
+                  <LogIn className="mr-2 h-5 w-5" aria-hidden="true" />
+                  Login
+                </>
+              )}
+            </button>
+          </form>
+
+          <p className="mt-8 text-center text-sm text-gray-700">
+            Not a member?{" "}
+            <Link
+              to="/signUp"
+              className="font-medium text-slate-700 hover:text-slate-500"
+            >
+              Sign up now
+              <ArrowRight className="inline h-4 w-4" />
+            </Link>
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 export default Login;
